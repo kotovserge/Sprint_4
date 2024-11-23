c2304e81675a0ac1a7cb99aa58b70246de05fdec
Sergey Kotov <s@ksmail.ru>
2024-11-23 14:47:21 +0300

    corrections after the review

--- src/main/java/pageObjects/BasePage.java
+++ src/main/java/pageObjects/BasePage.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-<<<<<<< HEAD
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
@@ -9,7 +8,4 @@ public abstract class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
-=======
-public class BasePage {
->>>>>>> 5cc22aa896feaf28c2b6a81d7e3af10c61362e14
 }
